c9974b925d9508beefd782bbc1df1ce3d50158ba
Michelle <micheellemendez2@gmail.com>
2022-08-25 17:58:17 -0500

    Practical test

--- Panaderia.py
+++ Panaderia.py
@@ -1,6 +1,6 @@
-// Una panadería vende barras de pan a 3.49€ cada una. El pan que no es el día tiene un descuento del 60 por ciento. 
-Escribe un programa que comience leyendo el número de barras vendidas que no son del día. 
-Después tu programa debe mostrar el precio habitual de una barra de pan, el descuento que se le hace por no ser fresca y el coste final total. //
+#// Una panadería vende barras de pan a 3.49€ cada una. El pan que no es el día tiene un descuento del 60 por ciento. 
+#E<scribe un programa que comience leyendo el número de barras vendidas que no son del día. 
+#Después tu programa debe mostrar el precio habitual de una barra de pan, el descuento que se le hace por no ser fresca y el coste final total. //#
 
 
 barras = int(input("Introduce el número de barras vendidas que no son frescas: "))
---
+++ horas.py
@@ -0,0 +1,4 @@
+horas = float(input("cuantas horas has trabajado?: "))
+cost= float(input("Cuanto cobras por hora?: "))
+paga = horas * cost
+print("Tu paga es", paga)
